bc69928a7cbb569117a6502cef6bab8dc17b7235
eugeniagarciaborghes <egarciaborghes@udesa.edu.ar>
2022-10-31 08:57:06 -0300

    Asi subimos desde la terminal att Nelson

---
+++ ProyectoReactNative1/src/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBh5XzVVnaCICuBGLxTw7PTUOd9W9HI7O4",
+  authDomain: "proyectoreactnative-29866.firebaseapp.com",
+  projectId: "proyectoreactnative-29866",
+  storageBucket: "proyectoreactnative-29866.appspot.com",
+  messagingSenderId: "762526211596",
+  appId: "1:762526211596:web:dc20b92d54fd7ed5dbd829"
+};
+
+const app = initializeApp(firebaseConfig);
